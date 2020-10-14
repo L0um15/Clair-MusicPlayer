fb464cdc7f39802695352cca907b80823ad6ee79
Damian Ziolo <damian.ziolo@onet.pl>
2020-10-14 19:05:30 +0200

    Split Musicplayer from main class

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,26 +20,18 @@ namespace Clair
 
         // Global Variables
         String version = "pre-0.7.0";
-        Double tempVolume;
-        String[] filePaths;
-        bool isMediaPlaying = false;
-        MediaPlayer mPlayer = new MediaPlayer();
-        Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
-        DispatcherTimer durationTimer = new DispatcherTimer();
+        Musicplayer musicplayer;
         TaskbarIcon taskbarIcon = new TaskbarIcon();
 
         public MainWindow()
         {
             InitializeComponent();
-
+            musicplayer = new Musicplayer(this);
             // Bind Events
-            mPlayer.MediaEnded += mPlayer_MediaEnded;
-            mPlayer.MediaOpened += mPlayer_MediaOpened;
-            durationTimer.Tick += dispatcherTimer_Tick;
+
             taskbarIcon.TrayLeftMouseDown += taskbar_TrayLeftMouseDown;
             this.StateChanged += Window_StateChanged;
             this.KeyDown += Window_OnKeyPressed;
-            durationTimer.Interval = new TimeSpan(0, 0, 1);
             volumeSlider.IsMoveToPointEnabled = true;
 
             blurBackground.Source = new BitmapImage(new Uri("assets/images/pixel.png", UriKind.RelativeOrAbsolute));
@@ -51,7 +43,7 @@ namespace Clair
             if (Settings.Default.isCheckingUpdates)
                 checkVersion();
 
-            if (Settings.Default.lastKnownDirectory != "nopath")
+            /*if (Settings.Default.lastKnownDirectory != "nopath")
             {
                 try {
                     if (!Settings.Default.isUnsupportedExtensionsEnabled)
@@ -80,7 +72,7 @@ namespace Clair
                 isMediaPlaying = false;
                 mPlayer.Pause();
 
-            }
+            }*/
 
         }
         private void checkVersion()
@@ -161,46 +153,8 @@ namespace Clair
             }
         }
 
-        private void dispatcherTimer_Tick(object sender, EventArgs args) {
 
-            // Changes Value of durationSlider
-
-            if (isMediaPlaying)
-            {
-                durationSlider.Value = (int) mPlayer.Position.TotalSeconds;
-            }
-        }
-
-        private void mPlayer_MediaOpened(object sender, EventArgs args) {
-
-            // Calls when mPlayer loads file
-
-            double duration = mPlayer.NaturalDuration.TimeSpan.TotalSeconds;
-            durationSlider.Maximum = (int)duration;
-            durationSlider.IsMoveToPointEnabled = true;
-            TimeSpan t = mPlayer.NaturalDuration.TimeSpan;
-            string time = t.ToString(@"mm\:ss");
-            totalTime.Text = time;
-            durationTimer.Start();
-        }
-
-        private void mPlayer_MediaEnded(object sender, EventArgs args) {
-
-            // Moves to next track if available
-
-            if (songList.SelectedIndex < songList.Items.Count - 1)
-                songList.SelectedIndex += 1;
-            else
-            {
-                // Hides pause button when there is no more tracks in queue
-                playButton.Visibility = Visibility.Visible;
-                pauseButton.Visibility = Visibility.Hidden;
-                isMediaPlaying = false;
-            }
-                
-        }
-
-        private void getArtistTagFromFile(String path) {
+        /*private void getArtistTagFromFile(String path) {
 
             // Get Artist and Title if available ...
 
@@ -216,10 +170,10 @@ namespace Clair
                 artistTitle.Content = null;
             }
             
-        }
+        }*/
 
 
-        private void getAlbumArtFromFile(int index,String path) {
+        /*private void getAlbumArtFromFile(int index,String path) {
 
             // Get Album cover if available
 
@@ -265,56 +219,16 @@ namespace Clair
                 else if (index == 2)
                     albumArtImage3.Source = new BitmapImage(new Uri("assets/images/noalbum.png", UriKind.RelativeOrAbsolute));
             }
-        }
+        }*/
 
         private void playButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (!isMediaPlaying) {
-                mPlayer.Play();
-                playButton.Visibility = Visibility.Hidden;
-                pauseButton.Visibility = Visibility.Visible;
-                isMediaPlaying = true;
-            }
+            musicplayer.Play();
         }
 
         private void folderButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            // Enables multiple selection
-            ofd.Multiselect = true;
-
-            // Shows only supported files when disabled
-            if (!Settings.Default.isUnsupportedExtensionsEnabled)
-                ofd.Filter = "Music files (*.mp3) | *.mp3";
-            else
-                ofd.Filter = "All files (*.*) | *.*";
-
-            if (ofd.ShowDialog() == true)
-            {
-                // Clears songList before adding to prevent duplication
-                if (songList.Items.Count > 0) {
-                    songList.Items.Clear();
-                    songListFiltered.Items.Clear();
-                }
-                // Adds file paths to array
-                filePaths = ofd.FileNames;
-
-                // Randomizes if enabled
-                if(Settings.Default.isAutoShuffleEnabled == true)
-                    randomizeSongSelection(filePaths);
-
-                // Converts file paths to normal ones
-                for (int i = 0; i < filePaths.Length; i++) {
-                    songList.Items.Add(System.IO.Path.GetFileNameWithoutExtension(filePaths[i]));
-                }
-
-                // Saves location for auto open feature
-                Settings.Default.lastKnownDirectory = System.IO.Path.GetDirectoryName(filePaths[0]);
-                Settings.Default.Save();
-
-                // Calls SelectionChanged Event
-                songList.SelectedIndex = 0;
-
-            }
+            musicplayer.openFileDialog();
         }
 
         private void songList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -326,8 +240,8 @@ namespace Clair
              * Every time when selection is changed, new track will be loaded ...
              * ... with equal index as position in filePaths array
              */
-
-            try
+            musicplayer.Prepare(musicplayer.filePaths[songList.SelectedIndex]);
+            /*try
             {
                 // Gets FILE PATH from filePaths and open it
                 mPlayer.Open(new Uri(filePaths[songList.SelectedIndex]));
@@ -348,11 +262,11 @@ namespace Clair
                     albumArtImage3.Source = new BitmapImage(new Uri("assets/images/noalbum.png", UriKind.RelativeOrAbsolute));
             }
             catch (IndexOutOfRangeException) { 
-                /*
+                *//*
                  * WinForms have SetSelected() method which uses try/catch for IndexOutOfRangeException
                  * After inspection of Listbox class i found out that catch field was blank
                  * So this field is ignored by me.
-                 */
+                 *//*
             }
             mPlayer.Play();
             if (!isMediaPlaying)
@@ -360,18 +274,16 @@ namespace Clair
                 playButton.Visibility = Visibility.Hidden;
                 pauseButton.Visibility = Visibility.Visible;
                 isMediaPlaying = true;
-            }
+            }*/
         }
 
         private void previousButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
             if (songList.SelectedIndex > 0)
-            {
                 songList.SelectedIndex -= 1;
-            }
         }
 
-        private void randomizeSongSelection<T>( T[] songPaths)
+        /*private void randomizeSongSelection<T>( T[] songPaths)
         {
 
             // Custom Randomize method
@@ -385,17 +297,11 @@ namespace Clair
             }
             
 
-        }
+        }*/
 
         private void stopButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (isMediaPlaying)
-            {
-                playButton.Visibility = Visibility.Visible;
-                pauseButton.Visibility = Visibility.Hidden;
-                mPlayer.Stop();
-                isMediaPlaying = false;
-            }
+            musicplayer.Stop();
         }
 
         private void listButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
@@ -435,12 +341,7 @@ namespace Clair
 
         private void pauseButton_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (isMediaPlaying) {
-                playButton.Visibility = Visibility.Visible;
-                pauseButton.Visibility = Visibility.Hidden;
-                mPlayer.Pause();
-                isMediaPlaying = false;
-            }
+            musicplayer.Pause();
         }
 
 
@@ -450,7 +351,7 @@ namespace Clair
              * Sets and saves value from volumeSlider to be used after closing application
              */
 
-            mPlayer.Volume = (double)volumeSlider.Value / 100;
+            musicplayer.setVolume(volumeSlider.Value / 100);
             Settings.Default.volumelevel = (int)volumeSlider.Value;
             Settings.Default.Save();
             Settings.Default.Reload();
@@ -460,25 +361,24 @@ namespace Clair
         {
             // Sets player position when Left Mouse button is released
 
-            mPlayer.Position = TimeSpan.FromSeconds((double)durationSlider.Value);
+            // mPlayer.Position = TimeSpan.FromSeconds((double)durationSlider.Value);
+            musicplayer.setPosition(TimeSpan.FromSeconds((double)durationSlider.Value));
         }
 
         private void durationSlider_DragCompleted(object sender, EventArgs e) {
 
             // Sets player position when drag is completed
 
-            mPlayer.Position = TimeSpan.FromSeconds((double)durationSlider.Value);
-            if(!durationTimer.IsEnabled)
-                durationTimer.Start();
+            /*mPlayer.Position = TimeSpan.FromSeconds((double)durationSlider.Value);*/
+            musicplayer.setPosition(TimeSpan.FromSeconds((double)durationSlider.Value));
+            musicplayer.StartDurationTimer();
         }
-        
+
         private void durationSlider_DragStarted(object sender, EventArgs e) {
 
             // Stop durationTimer to prevent changing value when dragging
 
-            if (durationTimer.IsEnabled) {
-                durationTimer.Stop();
-            } 
+            musicplayer.StopDurationTimer();
         }
 
         private void durationSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
@@ -500,39 +400,13 @@ namespace Clair
 
             // Randomize array, clear list and put randomized array back to list
 
-            if (songList.Items.Count > 0)
-            {
-                randomizeSongSelection(filePaths);
-                songList.Items.Clear();
-
-                for (int i = 0; i < filePaths.Length; i++)
-                {
-
-                    songList.Items.Add(System.IO.Path.GetFileNameWithoutExtension(filePaths[i]));
-
-                }
-
-                getAlbumArtFromFile(1, filePaths[0]);
-                getAlbumArtFromFile(2, filePaths[1]);
-
-            }
+            musicplayer.Shuffle();
         }
 
         private void volumeImage_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
 
-            if (mPlayer.Volume != 0.0D) {
-                tempVolume = mPlayer.Volume;
-                volumeSlider.Value = 0.0D;
-                mPlayer.Volume = 0.0D;
-                volumeImage.Opacity = 0.5;
-            }
-            else
-            {
-                mPlayer.Volume = tempVolume;
-                volumeSlider.Value = mPlayer.Volume * 100;
-                volumeImage.Opacity = 1;
-            }
+            musicplayer.Mute();
                 
         }
 
@@ -607,7 +481,7 @@ namespace Clair
 
             switch (e.Key)
             {
-                case Key.Space:
+                /*case Key.Space:
                     if (!isMediaPlaying)
                     {
                         mPlayer.Play();
@@ -631,7 +505,7 @@ namespace Clair
                         pauseButton.Visibility = Visibility.Hidden;
                         isMediaPlaying = false;
                     }
-                    break;
+                    break;*/
                 case Key.F1:
                     Process.Start("https://github.com/L0um15/Clair-MusicPlayer/issues/");
                     break;
---
+++ Musicplayer.cs
@@ -0,0 +1,195 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using Clair.Properties;
+using System.IO;
+using System.Windows.Threading;
+
+namespace Clair
+{
+    class Musicplayer
+    {
+        
+        MainWindow masterClass;
+        bool isMediaPlaying;
+        Double tempVolume;
+        public String[] filePaths;
+        OpenFileDialog ofd = new OpenFileDialog();
+        DispatcherTimer durationTimer = new DispatcherTimer();
+        MediaPlayer mPlayer = new MediaPlayer();
+
+        public Musicplayer(MainWindow mainWindow)
+        {
+            // Initialize.
+            masterClass = mainWindow;
+            mPlayer.MediaEnded += MPlayer_MediaEnded;
+            mPlayer.MediaOpened += MPlayer_MediaOpened;
+            durationTimer.Interval = new TimeSpan(0, 0, 1);
+            durationTimer.Tick += DurationTimer_Tick;
+        }
+
+        
+
+        public void Play()
+        {
+            mPlayer.Play();
+            if (!isMediaPlaying)
+            {
+                masterClass.playButton.Visibility = Visibility.Hidden;
+                masterClass.pauseButton.Visibility = Visibility.Visible;
+                isMediaPlaying = true;
+            }
+        }
+        public void Pause()
+        {
+            if (isMediaPlaying)
+            {
+                masterClass.playButton.Visibility = Visibility.Visible;
+                masterClass.pauseButton.Visibility = Visibility.Hidden;
+                mPlayer.Pause();
+                isMediaPlaying = false;
+            }
+        }
+        public void Stop()
+        {
+            if (isMediaPlaying)
+            {
+                masterClass.playButton.Visibility = Visibility.Visible;
+                masterClass.pauseButton.Visibility = Visibility.Hidden;
+                mPlayer.Stop();
+                isMediaPlaying = false;
+            }
+        }
+        public void Prepare(String path)
+        {
+            // Fetch list and prepare track.
+            try {
+                mPlayer.Open(new Uri(path));
+            }
+            catch (IndexOutOfRangeException) { }
+            Play();
+        }
+        public void Shuffle()
+        {
+            if(masterClass.songList.Items.Count > 0)
+            {
+                randomizeTrackSelection(filePaths);
+                masterClass.songList.Items.Clear();
+                for (int i = 0; i < filePaths.Length; i++)
+                {
+                    masterClass.songList.Items.Add(Path.GetFileNameWithoutExtension(filePaths[i]));
+                }
+            }
+        }
+        public void Mute()
+        {
+            if (mPlayer.Volume != 0.0D)
+            {
+                tempVolume = mPlayer.Volume;
+                masterClass.volumeSlider.Value = 0.0D;
+                mPlayer.Volume = 0.0D;
+                masterClass.volumeImage.Opacity = 0.5;
+            }
+            else
+            {
+                mPlayer.Volume = tempVolume;
+                masterClass.volumeSlider.Value = mPlayer.Volume * 100;
+                masterClass.volumeImage.Opacity = 1;
+            }
+        }
+        public void setVolume(double value)
+        {
+            mPlayer.Volume = value;
+        }
+        public void setPosition(TimeSpan value)
+        {
+            mPlayer.Position = value;
+        }
+        public void openFileDialog()
+        {
+            ofd.Multiselect = true;
+            if (ofd.ShowDialog() != true) return;
+            
+            if (!Settings.Default.isUnsupportedExtensionsEnabled)
+                ofd.Filter = "Music files (*.mp3) | *.mp3";
+            else
+                ofd.Filter = "All files (*.*) | *.*";
+
+            if (masterClass.songList.Items.Count > 0) {
+                masterClass.songList.Items.Clear();
+                masterClass.songListFiltered.Items.Clear();
+            }
+
+            filePaths = ofd.FileNames;
+
+            if (Settings.Default.isAutoShuffleEnabled)
+                randomizeTrackSelection(filePaths);
+
+            for (int i = 0; i < filePaths.Length; i++)
+            {
+                masterClass.songList.Items.Add(Path.GetFileNameWithoutExtension(filePaths[i]));
+            }
+
+            Settings.Default.lastKnownDirectory = Path.GetDirectoryName(filePaths[0]);
+            Settings.Default.Save();
+
+            masterClass.songList.SelectedIndex = 0;
+
+        }
+
+        public void StartDurationTimer()
+        {
+            if (!durationTimer.IsEnabled)
+                durationTimer.Start();
+        }
+        public void StopDurationTimer()
+        {
+            if (durationTimer.IsEnabled)
+                durationTimer.Stop();
+        }
+
+        private void randomizeTrackSelection<T>(T[] songPaths)
+        {
+            // Custom Randomize method
+            Random rand = new Random();
+            for (int i = 0; i < songPaths.Length; i++)
+            {
+                int j = rand.Next(i, songPaths.Length);
+                T tempPaths = songPaths[i];
+                songPaths[i] = songPaths[j];
+                songPaths[j] = tempPaths;
+            }
+        }
+
+        private void MPlayer_MediaOpened(object sender, EventArgs e)
+        {
+            double duration = mPlayer.NaturalDuration.TimeSpan.TotalSeconds;
+            masterClass.durationSlider.Maximum = (int)duration;
+            masterClass.durationSlider.IsMoveToPointEnabled = true;
+            TimeSpan t = mPlayer.NaturalDuration.TimeSpan;
+            string time = t.ToString(@"mm\:ss");
+            masterClass.totalTime.Text = time;
+            StartDurationTimer();
+        }
+
+        private void MPlayer_MediaEnded(object sender, EventArgs e)
+        {
+            if (masterClass.songList.SelectedIndex < masterClass.songList.Items.Count - 1)
+                masterClass.songList.SelectedIndex += 1;
+            else
+                Stop();
+        }
+        private void DurationTimer_Tick(object sender, EventArgs e)
+        {
+            if (isMediaPlaying)
+                masterClass.durationSlider.Value = (int)mPlayer.Position.TotalSeconds;
+        }
+
+    }
+}
